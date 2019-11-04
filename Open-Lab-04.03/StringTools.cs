2873431659d0888d457acaee55f75434b77945fc
Peterkoricar <korkosk123@gmail.com>
2019-11-04 11:34:46 +0100

    dfhds

--- Open-Lab-04.03/StringTools.cs
+++ Open-Lab-04.03/StringTools.cs
@@ -6,7 +6,19 @@ namespace Open_Lab_04._03
     {
         public string RemoveFirstLast(string original)
         {
-            throw new NotImplementedException();
+            string og = "";
+            string om = "";
+            
+            if (original.Length < 2)
+            {
+                return original;
+            }
+                og = original.Remove(0,1);
+                om = og.Remove(og.Length -1, 1);
+                
+               
+            
+            return om;
         }
     }
 }
